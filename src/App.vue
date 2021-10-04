9ab955d74f877023de127b37435d3eee85900472
Guilherme Torres <guiatorres8@gmail.com>
2021-10-04 20:27:49 -0300

    add some styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="flex justify-between max-w-xl mx-auto text-2xl">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/jokes">All Jokes</router-link>
     </div>
     <router-view />
   </div>
@@ -16,17 +16,15 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
-  padding: 30px;
+  padding: 60px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
 }
-
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: blue;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home shadow-xl bg-gray-200 w-8/12 mx-auto p-20">
+    <h2 class="text-3xl text-blue-600 font-extrabold">Can I Haz Dad Jokes</h2>
+    <h3 class="h-40 text-black">{{ joke }}</h3>
+    <button
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded-full
+        mt-10
+        focus:outline-none
+      "
+      @click="addJoke"
+    >
+      Add Joke
+    </button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+  name: 'Home',
+  components: {},
+}
 </script>
+
+<style scoped>
+.home {
+  height: 400px;
+  max-height: 500px;
+}
+h3 {
+  font-size: 2.5rem;
+}
+</style>
